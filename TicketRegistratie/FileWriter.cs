70c86bece7c1c6260b34534204f4c1c52eb6be93
Lee Beenen <l.beenen@diract.nl>
2014-02-15 16:02:12 +0100

    hoppa

---
+++ TicketRegistratie/FileWriter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace TicketRegistratie
+{
+  public static class FileWriter
+  {
+    private static readonly appsettings AppSettings;
+
+    static FileWriter()
+    {
+      AppSettings = new appsettings();
+    }
+    public static void WriteToFile(Probleem probleem)
+    {
+      var path = Path.Combine(AppSettings.OutputPath, AppSettings.FileName);
+      if (!File.Exists(path))
+      {
+        File.Create(path);
+      }
+      using (var streamWriter = File.AppendText(path))
+      {
+        streamWriter.WriteLine("--------------------------------------------------------------------");
+        streamWriter.WriteLine("Tijd/Datum  : {0}", DateTime.Now);
+        streamWriter.WriteLine("Probleem    : {0}", probleem.Issue);
+        streamWriter.WriteLine("Filiaal     : {0}", probleem.Filiaal);
+        streamWriter.WriteLine("Ticketnummer: {0}", probleem.Ticketnummer);
+        streamWriter.WriteLine("Oplossing   : {0}", probleem.Oplossing);
+        streamWriter.WriteLine("--------------------------------------------------------------------");
+        streamWriter.WriteLine();
+      }
+    }
+  }
+}
---
+++ TicketRegistratie/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace TicketRegistratie
+{
+  class Program
+  {
+    private static bool problemen = true;
+
+    static void Main(string[] args)
+    {
+      while (problemen)
+      {
+        Console.WriteLine("Is er een probleem?: Y/N");
+        problemen = isErEenProbleem(Console.ReadLine());
+        if (problemen)
+        {
+          ProbleemBuilder();
+        }
+      }
+    }
+
+    private static bool isErEenProbleem(string _input)
+    {
+      switch (_input.ToLower())
+      {
+        case "n":
+          {
+            return false;
+          }
+        default:
+          {
+            return true;
+          }
+      }
+    }
+
+    private static void ProbleemBuilder()
+    {
+      var probleem = new Probleem();
+
+      Console.WriteLine("Wat is het probleem, vriend?: ");
+      probleem.Issue = Console.ReadLine();
+      Console.WriteLine("Wat is het filiaal, vriend?: ");
+      probleem.Filiaal = Console.ReadLine();
+      Console.WriteLine("Is er een ticketnummer, vriend?: ");
+      probleem.Ticketnummer = Console.ReadLine();
+      Console.WriteLine("Wat heb je gedaan, vriend?: ");
+      probleem.Oplossing = Console.ReadLine();
+
+      FileWriter.WriteToFile(probleem);
+    }
+  }
+
+  public class Probleem
+  {
+    public string Issue { get; set; }
+    public string Filiaal { get; set; }
+    public string Ticketnummer { get; set; }
+    public string Oplossing { get; set; }
+  }
+}
